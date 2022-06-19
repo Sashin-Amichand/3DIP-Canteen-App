9c8c4359367de3e8b3286eff555b46feb8c17bf4
Void <sashin.amichand@icloud.com>
2022-06-19 20:21:44 +1200

    Added proper styling to make it look nicer, plus minor fixes.

--- canteen_app.py
+++ canteen_app.py
@@ -13,25 +13,21 @@ class MainApp(tk.Tk):
         '''Intializes tkinter and the basic window attributes.'''
         super().__init__()
         self.title('Canteen Orderng Applicaton')
-        self.geometry("500x170")
         # For when the user clicks the exit button
         self.protocol("WM_DELETE_WINDOW", self.close_window) 
 
         self.styling()
-
         self.configure(background=self.bg_color)
-        self.intro()
 
-    def intro(self):
-        '''Creates/Displays the introduction widgets.'''
-        self.window = ttk.LabelFrame(self, text='Welcome!', width=1000, 
-                                height=1000, style='LF.TLabelframe')
+        self.window = ttk.LabelFrame(self, text='Welcome!', width=620, height=200, style='TLabelframe')
         self.window.grid(row=0, column=0, padx=10, pady=10)
 
-        intro = "Welcome to my Canteen Ordering App! This app will allow you to\n place your order at the canteen, and you shall recieve a notification\n on when it is ready for you to pick up! If you wish to order click\n Student, if you work at the canteen then click Admin."
+        self.center_window(self.window)
+        self.x = 'hi'
+        intro = "Welcome to my Canteen Ordering App! This app will allow you to\n place your order at the canteen, and you shall recieve a notification\n on when it is ready for you to pick up! \nIf you wish to order click Student, if you work at the canteen then click Admin."
 
         intro_lbl = ttk.Label(self.window, text=intro, font=(self.txt_style), 
-                              justify='center', style='L.TLabel')
+                              justify='center', style='TLabel')
         intro_lbl.grid(row=1, columnspan=2, padx=5, pady=5)
 
         admin_btn = ttk.Button(self.window, text="Admin", 
@@ -46,32 +42,21 @@ class MainApp(tk.Tk):
         '''Simple method to display the window of the users choice.'''
         if chosen_window is not None:
             self.withdraw()
-            show_window = tk.Toplevel(self)
-            chosen_window(show_window)
+            new_window = tk.Toplevel(self)
+            chosen_window(new_window)
 
-    def styling(self): 
-        '''Creates the variables used for styling'''
-        self.bg_color = '#61002f'
-        self.frm_style = 'Cormorant', '18', 'bold'
-        self.txt_style = 'Cormorant', '12'
-        self.txt_color = "#89c9ec"
+    def center_window(self, window=None):
+        if window is not None:
+            self.window = window
+            self.window.update_idletasks() #Add this line
 
-        self.MLFrame = ttk.Style()
-        self.MLFrame.configure('LF.TLabelframe', background=self.bg_color)
-        self.MLFrame.configure('LF.TLabelframe.Label', 
-                    background=self.bg_color, foreground=self.txt_color, 
-                    font=(self.frm_style))
+            width = self.window.winfo_width()
+            height = self.window.winfo_height()
+            x = (self.window.winfo_screenwidth() // 2) - (width // 2)
+            y = (self.window.winfo_screenheight() // 2) - (height // 2)
 
-        self.TLabel = ttk.Style()
-        self.TLabel.configure('L.TLabel', background=self.bg_color, 
-                              foreground=self.txt_color)
-
-        self.TLabel = ttk.Style()
-        self.TLabel.configure('L.TLabel', background=self.bg_color, 
-                              foreground=self.txt_color)
-        
-        self.TEntry = ttk.Style()
-        self.TEntry.configure("EntryStyle.TEntry",)
+            self.geometry('{}x{}+{}+{}'.format(width, height, x, y))
+        return
 
     def close_window(self): 
         '''Required for when the user wishes to close the program'''
@@ -84,16 +69,66 @@ class MainApp(tk.Tk):
         self.parent.withdraw()
         app.deiconify()
 
+    def cafe_menu(self):
+        '''This method opens up the cafe menu file, and reads it and saves the contents in a variable.'''
+        with open("cafe_menu.json") as file:
+            contents = json.load(file)
+        self.menu_items = contents["menu_items"][0]
+
+    def styling(self): 
+        '''Creates the variables used for styling'''
+        self.bg_color = '#61002f'
+        self.frm_style = 'Cormorant', '20', 'bold'
+        self.btn_style = 'Cormorant', '13', 'bold'
+        self.txt_style = 'Cormorant', '13'
+        self.txt_color = "#89c9ec"
+        self.btn_color = "#28a8ed"
+
+        self.style = ttk.Style()
+        self.style.theme_use('clam')
+
+        self.TLF = ttk.Style()
+        self.TLF.theme_use('alt')
+        self.TLF.configure('TLabelframe', background=self.bg_color, 
+                            borderwidth=10, relief='raised')
+        self.TLF.configure('TLabelframe.Label', 
+                    background=self.bg_color, foreground=self.txt_color, 
+                    font=(self.frm_style))
+
+        self.TLabel = ttk.Style()
+        self.TLabel.configure('TLabel', background=self.bg_color, 
+                              foreground=self.txt_color)
+
+        self.TEntry = ttk.Style()
+        self.TEntry.configure("EntryStyle.TEntry", )
+
+        self.TButton = ttk.Style()
+        self.TButton.configure('TButton', background=self.btn_color,
+                                foreground='black', font=(self.btn_style))
+        self.TButton.configure('Del.TButton', background=self.btn_color, 
+                                foreground='black')
+        self.TButton.configure('Add.TButton', background=self.btn_color,
+                              foreground='black')
+
+        self.TButton.map('TButton', background=[('active', self.bg_color)], 
+                        foreground=[('active', self.btn_color)])
+        self.TButton.map('Del.TButton', background=[('active','red')], foreground=[('active', 'white')])
+        self.TButton.map('Add.TButton', background=[('active','green')], 
+                        foreground=[('active', 'white')])
+        
 
 class Student(tk.Frame, MainApp):
     '''The class that models the student window/ordering processes.'''
     def __init__(self, parent):
         '''Intiliazes the styling and variables used for the order widgets.'''
         super().__init__(parent)
+    
         self.parent = parent
         self.parent.protocol("WM_DELETE_WINDOW", self.close_window)
 
         self.styling()
+        self.cafe_menu()
+
         self.parent.configure(background=self.bg_color)
 
         # Variables storing cost/menu item/food in a category and currently ordered food
@@ -106,34 +141,31 @@ class Student(tk.Frame, MainApp):
         self.total_quantity = {}
         self.current_category_cost = []
         self.total_category_cost = {}
+
         # Variable for the storing individual category costs and then the total cost itself.
         self.final_cost = {}
         self.total = 0
 
-        # Opens cafe menu file, and saves it in a variable.
-        with open("cafe_menu.json") as file:
-            contents = json.load(file)
-        self.menu_items = contents["menu_items"][0]
-
         # Frames set
         self.about_frm = ttk.LabelFrame(self.parent, text='Order:', width=700,
-                                    height=700, style='LF.TLabelframe')
+                                    height=700, style='TLabelframe')
         self.menu_frm = ttk.LabelFrame(self.parent, text='Menu:',
-                                        style='LF.TLabelframe')
+                                        style='TLabelframe')
         self.order_frm = ttk.LabelFrame(self.parent, text='Order Items:', 
                                         width=700, height=700, 
-                                        style='LF.TLabelframe')
+                                        style='TLabelframe')
 
         self.about_frm.grid(row=0, columnspan=8, padx=10)
-        self.menu_frm.grid(row=1, columnspan=8, padx=10)
+        self.menu_frm.grid(row=1, columnspan=8, padx=20)
+
         self.order()
 
     def order(self):
-        '''Method displaying what this window is, and the category of food.'''
+        '''Method displaying what this window is about, and the category of food.'''
 
         order = "Please choose the items you would like to order, \nfrom the categories below:"
         order_lbl = ttk.Label(self.about_frm, text=order, justify='center',
-                             font=(self.txt_style), style='L.TLabel')
+                             font=(self.txt_style), style='TLabel')
         order_lbl.grid(row=1, columnspan=8, padx=5, pady=5)
 
         separator = ttk.Separator(self.about_frm, orient='horizontal')
@@ -189,15 +221,15 @@ class Student(tk.Frame, MainApp):
 
             item_lbl = ttk.Label(self.menu_frm, text=self.items_txt,
                                 font=(self.txt_style), justify='left',
-                                style='L.TLabel')
+                                style='TLabel')
 
             quantity_btn = ttk.Button(self.menu_frm, text="Add 1x", 
                         command=lambda item_number=item_number:  
-                        self.create_cost(item_number))
+                        self.create_cost(item_number), style='Add.TButton')
 
-            remove_btn = ttk.Button(self.menu_frm, text="Remove item", 
+            remove_btn = ttk.Button(self.menu_frm, text="Remove 1x", 
                         command=lambda del_item=del_item: 
-                        self.create_cost(del_item, True))
+                        self.create_cost(del_item, True), style='Del.TButton')
 
             item_lbl.grid(row=rows, column=1, padx=5, pady=5)
             quantity_btn.grid(row=rows, column=2, padx=5, pady=5, sticky="NW")
@@ -254,35 +286,33 @@ class Student(tk.Frame, MainApp):
         '''First grabs the total cost of the users order, then displays the total cost and items ordered.'''
         cost = f"The total cost is ${self.total}0!"
         cost_lbl = ttk.Label(self.order_frm, text=cost,
-                            justify='center', font=(self.txt_style), 
-                            style='L.TLabel')
+                            justify='center', font=('Cormorant', '14', 'bold'), 
+                            style='TLabel')
         ordered = f'You have ordered: {", ".join(self.food_ordered)}'
         ordered_lbl = ttk.Label(self.order_frm, text=ordered,
-                            justify='center', font=(self.txt_style), 
-                            style='L.TLabel')
+                            justify='center', font=('Cormorant', '12', 'bold'), 
+                            style='TLabel')
 
         self.order_frm.grid(row=2, columnspan=8, padx=10)
-        cost_lbl.grid(row=1, column=1, padx=5, pady=5)
-        ordered_lbl.grid(row=2, column=1, padx=5, pady=5)
+        ordered_lbl.grid(row=1, column=1, padx=5, pady=5)
+        cost_lbl.grid(row=2, column=1, padx=5, pady=5)
 
 class Admin(tk.Frame, MainApp):
     def __init__(self, parent):
         super().__init__(parent)
         self.parent = parent
-        self.parent.geometry("440x210")
         self.parent.protocol("WM_DELETE_WINDOW", self.close_window)
 
         self.styling()
         self.parent.configure(background=self.bg_color)
-        self.menu_items = MainApp.open_cafe_menu.menu_items
-        print(self.menu_items)
+
         self.login_frm = ttk.LabelFrame(self.parent, text='Login:', width=500,
-                                    height=500, style='LF.TLabelframe')
+                                    height=500, style='TLabelframe')
         self.login_frm.grid(row=0, columnspan=3, padx=10, pady=10)
 
         login = "If you are a canteen staff worker, please enter your\n credentials below."
         login_lbl = ttk.Label(self.login_frm, text=login, justify='center',
-                             font=(self.txt_style), style='L.TLabel')
+                             font=(self.txt_style), style='TLabel')
         login_lbl.grid(row=1, columnspan=3, padx=5, pady=5)
         separator = ttk.Separator(self.login_frm, orient='horizontal')
         separator.grid(row=2, columnspan=3, ipadx=170, padx=5, pady=5)
@@ -295,9 +325,9 @@ class Admin(tk.Frame, MainApp):
         self.check = tk.IntVar()
 
         username_lbl = ttk.Label(self.login_frm, text='Username:',
-                                font=(self.txt_style), style='L.TLabel')
+                                font=(self.txt_style), style='TLabel')
         password_lbl = ttk.Label(self.login_frm, text='Password:',
-                                font=(self.txt_style), style='L.TLabel')
+                                font=(self.txt_style), style='TLabel')
         username_lbl.grid(row=3, column=0, padx=5, pady=5)
         password_lbl.grid(row=4, column=0, padx=5, pady=5)
 
@@ -319,11 +349,11 @@ class Admin(tk.Frame, MainApp):
         show_password.grid(row=4, column=2, padx=5)
 
         back_btn = ttk.Button(self.login_frm, text="Go Back", 
-                               command=lambda: self.go_back())
+                               command=lambda: self.go_back(), width=10)
         login_btn = ttk.Button(self.login_frm, text="Sign In", 
-                               command=lambda: self.sign_in())
-        back_btn.grid(row=5, column=1, pady=5, sticky="NW")
-        login_btn.grid(row=5, column=1, pady=5, sticky="NE")
+                               command=lambda: self.sign_in(), width=10)
+        back_btn.grid(row=5, column=1, padx=2, pady=5, sticky="NE")
+        login_btn.grid(row=5, column=2, pady=5, sticky="Ne")
 
     def show_pass(self, password_entry):
         if self.check.get() == 1 :
